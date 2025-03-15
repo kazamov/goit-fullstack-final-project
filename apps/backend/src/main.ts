d155035c4c23d598617d40db17307924136673e6
Zakir Nuriiev <zakir.nuriiev@innervate.com>
2025-03-15 23:45:10 +0200

    update pipeline

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
@@ -6,6 +7,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (_req, res) => {
   res.status(200).send('Health check');
