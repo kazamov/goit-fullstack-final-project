aba1ae89e26b573c33586d22230c30c27a970e1d
Zakir Nuriiev <zakir.nuriiev@innervate.com>
2025-03-15 14:20:27 +0200

    add healthcheck, remove frontend app content

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,8 +5,10 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.status(200).send('Health check');
 });
 
 app.listen(port, host, () => {
