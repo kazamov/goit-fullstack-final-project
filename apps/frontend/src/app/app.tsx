47e55270534ea1282fce0cdcf1c7f789c93f5955
Zakir Nuriiev <zakir.nuriiev@innervate.com>
2025-03-16 00:15:28 +0200

    call api from the frontend

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,5 +1,33 @@
+import { useEffect, useState } from 'react';
+
 export function App() {
-  return <div>APP CONTENT</div>;
+  const [succeeded, setSucceeded] = useState(false);
+
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.error(
+          'There has been a problem with your fetch operation:',
+          error
+        );
+      });
+  }, []);
+
+  return (
+    <>
+      <div>APP CONTENT</div>
+      {succeeded && <div>SUCCEEDED TO CALL API</div>}
+    </>
+  );
 }
 
 export default App;
