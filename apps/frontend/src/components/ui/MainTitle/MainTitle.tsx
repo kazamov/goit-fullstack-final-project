74a3a588c00fc86c2434f924fe60a87feeb7ed82
Julia Cheperis <julia.cheperis@dataart.com>
2025-03-22 18:15:52 +0100

    add title and subtitle

--- apps/frontend/src/components/ui/MainTitle/MainTitle.tsx
+++ apps/frontend/src/components/ui/MainTitle/MainTitle.tsx
@@ -1,5 +1,11 @@
-const MainTitle = () => {
-  return <div></div>;
+import styles from './MainTitle.module.css';
+
+interface MainTitleProps {
+  title: string;
+}
+
+const MainTitle = ({ title }: MainTitleProps) => {
+  return <h2 className={styles.mainTitle}>{title}</h2>;
 };
 
 export default MainTitle;
--- apps/frontend/src/components/ui/Subtitle/Subtitle.tsx
+++ apps/frontend/src/components/ui/Subtitle/Subtitle.tsx
@@ -1,5 +1,11 @@
-const SubTitle = () => {
-  return <div></div>;
+import styles from './SubTitle.module.css';
+
+interface SubTitleProps {
+  title: string;
+}
+
+const SubTitle = ({ title }: SubTitleProps) => {
+  return <h3 className={styles.subTitle}>{title}</h3>;
 };
 
 export default SubTitle;
