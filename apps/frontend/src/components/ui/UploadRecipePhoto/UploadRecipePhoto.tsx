b92dd700665179f18001eaf500c864250ab38e8e
Olexandrd <olexandrd@gmail.com>
2025-03-24 21:20:22 +0200

    feat: add recipe photo uploader component

---
+++ apps/frontend/src/components/ui/UploadRecipePhoto/UploadRecipePhoto.tsx
@@ -0,0 +1,56 @@
+import type { ChangeEvent } from 'react';
+import { useState } from 'react';
+
+import styles from './UploadRecipePhoto.module.css';
+
+const UploadRecipePhoto = () => {
+  const [image, setImage] = useState<File | null>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setPreview(reader.result as string);
+    };
+    reader.readAsDataURL(selectedImage);
+  };
+
+  return (
+    <div className={styles.uploadRecipePhoto}>
+      <label className={styles.uploadLabel}>
+        {preview ? (
+          <img className={styles.previewImage} src={preview} alt="Preview" />
+        ) : (
+          <>
+            <svg
+              className={styles.uploadIcon}
+              onClick={() =>
+                (
+                  document.querySelector(
+                    `.${styles.uploadInput}`,
+                  ) as HTMLInputElement
+                )?.click()
+              }
+            >
+              <use href={`/icons.svg#icon-camera`}></use>
+            </svg>
+            <p className={styles.noFileSelectedText}>Upload a photo</p>
+          </>
+        )}
+        <input
+          className={styles.uploadInput}
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
+      </label>
+    </div>
+  );
+};
+
+export default UploadRecipePhoto;
