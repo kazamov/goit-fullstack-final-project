9825ebc6d00e6d9b39aa381e15172c43c4032d9e
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2025-03-27 21:33:03 +0100

    Export Images  as a separate component

--- apps/frontend/src/components/modules/Common/Hero/Hero.tsx
+++ apps/frontend/src/components/modules/Common/Hero/Hero.tsx
@@ -8,6 +8,8 @@ import ButtonHero from '../../../ui/ButtonHero/ButtonHero';
 import SignInModal from '../../Auth/SignInModal/SignInModal';
 import SignUpModal from '../../Auth/SignUpModal/SignUpModal';
 
+import HeroImages from './HeroImages';
+
 import styles from './Hero.module.css';
 
 const Hero = () => {
@@ -50,25 +52,7 @@ const Hero = () => {
           Add recipe
         </ButtonHero>
 
-        <div className={styles.imageWrapper}>
-          <picture className={styles.firstImage}>
-            <source
-              type="image/webp"
-              srcSet="/images/hero/hero-cake.webp 1x, /images/hero/hero-cake@2x.webp 2x"
-              media="(max-width: 767px)"
-            />
-            <img src="/images/hero/hero-cake.webp" alt="Cake on a plate" />
-          </picture>
-
-          <picture className={styles.secondImage}>
-            <source
-              type="image/webp"
-              srcSet="/images/hero/hero-meat.webp 1x, /images/hero/hero-meat@2x.webp 2x"
-              media="(max-width: 767px)"
-            />
-            <img src="/images/hero/hero-meat.webp" alt="Meat roll on a plate" />
-          </picture>
-        </div>
+        <HeroImages></HeroImages>
       </div>
 
       {/* Modals are always mounted and controlled via Redux */}
---
+++ apps/frontend/src/components/modules/Common/Hero/HeroImages.tsx
@@ -0,0 +1,27 @@
+import styles from './Hero.module.css';
+
+const HeroImages = () => {
+  return (
+    <div className={styles.imageWrapper}>
+      <picture className={styles.firstImage}>
+        <source
+          type="image/webp"
+          srcSet="/images/hero/hero-cake.webp 1x, /images/hero/hero-cake@2x.webp 2x"
+          media="(max-width: 767px)"
+        />
+        <img src="/images/hero/hero-cake.webp" alt="Cake on a plate" />
+      </picture>
+
+      <picture className={styles.secondImage}>
+        <source
+          type="image/webp"
+          srcSet="/images/hero/hero-meat.webp 1x, /images/hero/hero-meat@2x.webp 2x"
+          media="(max-width: 767px)"
+        />
+        <img src="/images/hero/hero-meat.webp" alt="Meat roll on a plate" />
+      </picture>
+    </div>
+  );
+};
+
+export default HeroImages;
