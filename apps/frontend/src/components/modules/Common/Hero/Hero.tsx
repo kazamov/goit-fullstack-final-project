77788d1da518f7882a33bf3fb006e9cbc693f524
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2025-03-28 09:00:28 +0100

    Fix: use existing selector

--- apps/frontend/src/components/modules/Common/Hero/Hero.tsx
+++ apps/frontend/src/components/modules/Common/Hero/Hero.tsx
@@ -2,8 +2,9 @@ import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-import type { AppDispatch, RootState } from '../../../../redux/store';
+import type { AppDispatch } from '../../../../redux/store';
 import { setModalOpened } from '../../../../redux/ui/slice';
+import { selectCurrentUser } from '../../../../redux/users/selectors';
 import ButtonHero from '../../../ui/ButtonHero/ButtonHero';
 import SignInModal from '../../Auth/SignInModal/SignInModal';
 import SignUpModal from '../../Auth/SignUpModal/SignUpModal';
@@ -16,9 +17,7 @@ const Hero = () => {
   const dispatch = useDispatch<AppDispatch>();
   const navigate = useNavigate();
 
-  const currentUser = useSelector(
-    (state: RootState) => state.users.currentUser,
-  );
+  const currentUser = useSelector(selectCurrentUser);
 
   const openSignInModal = useCallback(() => {
     dispatch(setModalOpened({ modal: 'login', opened: true }));
@@ -52,7 +51,7 @@ const Hero = () => {
           Add recipe
         </ButtonHero>
 
-        <HeroImages></HeroImages>
+        <HeroImages />
       </div>
 
       {/* Modals are always mounted and controlled via Redux */}
